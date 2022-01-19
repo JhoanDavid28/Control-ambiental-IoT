0a15bd81e124940cd5f6f72cff90e3f6dc9af580
Jhoan David <pjhoandavid@outlook.com>
2022-01-19 01:11:48 -0500

    Consulta de datos del bot

--- Script para el server/bot_IoT.py
+++ Script para el server/bot_IoT.py
@@ -1,5 +1,33 @@
+# Importamos paquetes que necesitamos
+import os
+import tweepy
+import requests
+import time
 
 
+#toquen para enviar al bot y conectar con twitter
+CONSUMER_KEY=''
+CONSUMER_SECRET=''
+ACCESS_TOKEN=''
+ACCESS_TOKEN_SECRET=''
+
+def consultaDeDatos():
+    # Consulta los datos del sensor en la IP del Wemos
+    response = requests.get('http://192.168.0.24:80')
+    response2 = requests.get('http://192.168.0.27:80')
+
+    # Convierte la respuesta del servidor de Wemos en un diccionario de Python
+    sensor_data = response.json()
+    sensor_data2 = response2.json()
+    
+    temperature = sensor_data['variables']['temperature']
+
+    humidity = sensor_data['variables']['humidity']
+    pollution = sensor_data['variables']['contaminacion']
+
+    temperature2 = sensor_data2['variables']['temperature']
+    humidity2 = sensor_data2['variables']['humidity']
+    pollution2 = sensor_data2['variables']['contaminacion']
 
 def main():
     pass
