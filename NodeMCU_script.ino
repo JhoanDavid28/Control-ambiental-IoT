d5a3320f6a010a959b7a7f6581441f3fb34e9c5a
Jhoan David <pjhoandavid@outlook.com>
2022-01-18 21:23:08 -0500

    Definido las entradas a usar

---
+++ NodeMCU_script.ino
@@ -0,0 +1,14 @@
+// Librerias
+#include "ESP8266WiFi.h"
+#include <aREST.h>
+#include <DHT.h>
+#include <DHT_U.h>
+// Libreia master 
+#include <Adafruit_Sensor.h>
+
+// Definir las entradas
+#define DHTPIN D2
+#define DHTTYPE DHT22
+#define MQ A0
+
+
