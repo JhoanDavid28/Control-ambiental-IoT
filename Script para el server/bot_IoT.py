a25f1a31247c0c1f202a7f0ed329e9506a4096b9
Jhoan David <pjhoandavid@outlook.com>
2022-02-02 00:52:26 -0500

    obtencion y almacenamiento de datos de los sensores

--- Script para el server/bot_IoT.py
+++ Script para el server/bot_IoT.py
@@ -1,7 +1,7 @@
 # Importamos paquetes que necesitamos
-import os
-import tweepy
+import json
 import requests
+import datetime
 import time
 
 
@@ -12,48 +12,50 @@ ACCESS_TOKEN=''
 ACCESS_TOKEN_SECRET=''
 
 #IP de los sensores en red local
-IP_SENSOR_OUTDOOR = 'http://192.168.0.24:80'
-IP_SENSOR_INDOOR = 'http://192.168.0.27:80'
+IP_SENSOR_OUTDOOR = 'http://192.168.101.35:80/'
+IP_SENSOR_INDOOR = 'http://192.168.101.36:80/'
 
-def obtenerDatosDeSensorOutdoor():
-    # Consulta los datos del sensor en la IP del Wemos
-    response = requests.get(IP_SENSOR_OUTDOOR)
-    # Convierte la respuesta del servidor de NodeMCU en un diccionario de Python
-    sensor_data = response.json()
-    
-    temperature = sensor_data['variables']['temperature']
-    humidity = sensor_data['variables']['humidity']
-    pollution = sensor_data['variables']['contaminacion']
+mediciones = {}
+mediciones2 = {}
+a = 0
+def obtenerDatosDeSensores():
+    while True:
+        time.sleep(10)
+        # Consulta los datos del sensor en la IP del
+        response = requests.get(IP_SENSOR_OUTDOOR)
+        # Convierte la respuesta del servidor de NodeMCU en un diccionario de Python
+        sensor_data = response.json()
+        print(sensor_data)
+
+        response2 = requests.get(IP_SENSOR_INDOOR)
+        sensor_data2 = response2.json()
+        print(sensor_data2)
+
+        ahora = datetime.datetime.now()
+        sensor_data["fecha"] = ahora.strftime('%d/%m/%Y')
+        sensor_data["hora"] = ahora.strftime('%H:%M:%S')
+
+        sensor_data2["fecha"] = ahora.strftime('%d/%m/%Y')
+        sensor_data2["hora"] = ahora.strftime('%H:%M:%S')
+
+        tf = open("myDictionary_outdoor.json", "w")
+        mediciones['dic' + str(a)] = sensor_data
+        json.dump(mediciones, tf)
+        tf.close()
+
+        tf2 = open("myDictionary2_indoor.json", "w")
+        mediciones2['dic' + str(a)] = sensor_data2
+        json.dump(mediciones2, tf2)
+        tf.close()
+
+        a += 1
 
 
-def obtenerDatosDeSensorIndoor():
-    # Consulta los datos del sensor en la IP del Wemos
-    response = requests.get(IP_SENSOR_OUTDOOR)
 
-    # Convierte la respuesta del servidor de NodeMCU en un diccionario de Python
-    sensor_data = response.json()
-    
-    temperature = sensor_data['variables']['temperature']
-    humidity = sensor_data['variables']['humidity']
-    pollution = sensor_data['variables']['contaminacion']
-
-
-def guardarDatosOutdoor():
-    sensor_outdoor = "C:\\Users\\" + os.getlogin() + "\\Desktop\\"
-    a = open(sensor_outdoor + "Datos obtenidos de NodeMCU exterior.txt", "w")
-    a.write("\n".join(sensor_outdoor))
-    a.close()
-
-def guardarDatosIndoor():
-    sensor_indoor= "C:\\Users\\" + os.getlogin() + "\\Desktop\\"
-    a = open(sensor_indoor + "Datos obtenidos de NodeMCU interior.txt", "w")
-    a.write("\n".join(sensor_indoor))
-    a.close()
 
 
 def main():
-    obtenerDatosDeSensorIndoor()
-    obtenerDatosDeSensorOutdoor()
+    obtenerDatosDeSensores()
 
 
 if __name__ == "__main__":
